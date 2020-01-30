5f4c480c01559f5d19032a1c68673754aabecce5
madpro101 <madivanitinashe@gmail.com>
2020-01-30 22:21:24 +0200

    initial commit

---
+++ dungeon_game.py
@@ -0,0 +1,31 @@
+import random
+# To do 30 January 2020(Nhasi)
+
+# Draw Grid 
+# Pick Random Location for player
+# Pick Random Location for exit door
+# Pick Random Location for monster
+# Draw player in the Grid
+# take input for movement 
+# move player unless invalid move(past edges of grid)
+# check for if player won or lost
+# clear screen and redraw grid  
+
+
+CELLS = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), 
+         (0, 1), (1, 1), (2, 1), (3, 1), (4, 1), 
+         (0, 2), (1, 2), (2, 2), (3, 2), (4, 2), 
+         (0, 3), (1, 3), (2, 3), (3, 3), (4, 3), 
+         (0, 4), (1, 4), (2, 4), (3, 4), (4, 4),
+         (0, 5), (1, 5), (2, 5), (3, 5), (4, 5)
+         ]
+
+# Function to get the locations 
+def get_location():
+     monster = None
+     door = None
+     player = None
+    
+return monster, player, door
+     
+
