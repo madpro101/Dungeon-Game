4499598ccc8f7b8b4885fdb47fdf0b6a9e99eddb
madpro101 <madivanitinashe@gmail.com>
2020-02-03 14:59:07 +0200

    Game logic now  working needing a small fix

--- dungeon_game.py
+++ dungeon_game.py
@@ -1,3 +1,4 @@
+import os
 import random
 # To do by the time i finish making the game
 
@@ -17,31 +18,92 @@ CELLS = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0),
          (0, 2), (1, 2), (2, 2), (3, 2), (4, 2), 
          (0, 3), (1, 3), (2, 3), (3, 3), (4, 3), 
          (0, 4), (1, 4), (2, 4), (3, 4), (4, 4),
-         (0, 5), (1, 5), (2, 5), (3, 5), (4, 5)
          ]
-
+# clear screen function 
+def clear_screen():
+     os.system('cls' if os.name =='nt' else 'clear')
 # Function to get the locations 
-def get_location():
-     monster = None
-     door = None
-     player = None
-    
-	return monster, player, door
+def get_location(): 
+	return random.sample(CELLS, 3)
 
-# Game loop
-while True:
-     print("Welcome to the Dungeon. A game proudly made by Dveelopers for Developers")
-     print("You are currently in room {}") #fill in position where player is in 
-     print("You can move {}") # fill with available moves
-     print("Enter QUIT to quit game")
+def move_player(player, move):
+     x, y = player
+     if move == 'LEFT':
+          x-=1
+     if move == 'RIGHT':
+          x+=1
+     if move == 'UP':
+          y-=1
+     if move == 'DOWN':
+          y+=1
+     return x, y
+    
+def get_moves(player):
+     moves = ["LEFT", "RIGHT", "UP", "DOWN"]
+     x, y = player #unpacking player tuple into x, y cordinates 
+     if x == 0:
+          moves.remove('LEFT')
+     if x == 4:
+          moves.remove('RIGHT')  
+     if y == 0:
+          moves.remove('UP')
+     if y == 4:
+          moves.remove('DOWN')
+     return moves
+
+def draw_map(player):
+     print(" _"*5)
+     tile = "|{}"
      
-     move = input("> " ).upper()
+     for cell in CELLS:
+          x, y = cell
+          if x < 4:
+               line_end = ""
+               if cell == player:
+                    output = tile.format("X")
+               else:
+                    output = tile.format("_")
+          else:
+               line_end = "\n"
+               if cell == player:
+                    output = tile.format("{}|")
+               else:
+                    output = tile.format("_|")
+                    
+          print(output, end = line_end)
+def game_loop():
      
-     if move == "QUIT":
-          break
-     
-     # Good move? Change player position
-     # Bad move? Do not change player position
-     # On the door? They win 
+     monster, door, player = get_location()  
+     while True:
+          draw_map(player)
+          valid_moves= get_moves(player)
+          print("You are currently in room {}".format(player)) #fill in position where player is in 
+          print("You can move {}".format(", ".join(valid_moves))) # fill with available moves
+          print("Enter QUIT to quit game")
+          
+          move = input("> " ).upper()
+          
+          if move == "QUIT":
+               break
+          if move in valid_moves:
+               player = move_player(player, move)
+               if player == 'monster':
+                    print("\n ** Ooops the angry potato and vegy eating monster got you**\n")
+                    
+                    break
+               if player== 'door':
+                    print("**\n hurray you won. Come get your prize**\n")
+                    break
+          else:
+               input("\n**You can't go there**\n")
+          clear_screen()
+             
+
+clear_screen
+print("Welcome to the Dungeon. ")
+input("Press Enter to continue")
+clear_screen()
+game_loop()
+
      
      
